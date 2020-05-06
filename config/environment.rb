653ef46bc279704580e5fcacb0324b65fb27d151
Johntremont <piloto2810@gmail.com>
2020-05-06 15:19:35 -0400

    seteando primeros modelos

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,23 @@
 # Load the Rails application.
 require_relative 'application'
+require 'awesome_print'
+# false
+
+test_data = {:a => 'Ruby in Rails String', :b => {:array => [1,2,3]}}
+
+# {:a => 'Ruby in Rails String', :b => {:array => [1,2,3]}}
+
+ap test_data
+# {
+#     :a => "Ruby in Rails String",
+#     :b => {
+#         :array => [
+#             [0] 1,
+#             [1] 2,
+#             [2] 3
+#         ]
+#     }
+# }
 
 # Initialize the Rails application.
 Rails.application.initialize!
